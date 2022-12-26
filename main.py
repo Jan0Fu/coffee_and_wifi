bcd824b9b9181e60904dbc7a81537e5d81a19b5a
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-26 10:20:37 +0100

    Improved functionality

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField
-from wtforms.fields.html5 import URLField, TimeField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
-import pandas
 
-data_file = pandas.read_csv("cafe-data.csv")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -16,24 +13,15 @@ Bootstrap(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = URLField('Location', validators=[DataRequired(), URL()])
-    open_time = TimeField('Open time', validators=[DataRequired()])
-    close_time = TimeField('Close time', validators=[DataRequired()])
-    coffee_rating = RadioField('Coffee rating', choices=["☕", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
-    wifi_rating = RadioField('WiFi rating', choices=["💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
-    power_rating = RadioField('Power rating', choices=["🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    location = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    open_time = StringField('Opening Time e.g. 8AM', validators=[DataRequired()])
+    close_time = StringField('Closing Time e.g. 5:30PM', validators=[DataRequired()])
+    coffee_rating = SelectField('Coffee Rating', choices=["☕", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
+    wifi_rating = SelectField('WiFi Strength Rating', choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+    power_rating = SelectField('Power Socket Availability', choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
-
-# all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -43,9 +31,15 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        with open("cafe-data.csv", "w") as file:
-            file.write(form.data)
-        print("Invalid input")
+        with open("cafe-data.csv", mode="a") as file:
+            file.write(f"\n{form.data.cafe},"
+                       f"{form.data.location},"
+                       f"{form.data.open_time},"
+                       f"{form.data.close_time},"
+                       f"{form.data.coffee_rating},"
+                       f"{form.data.wifi_rating},"
+                       f"{form.data.power_rating}")
+        return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
